4591e7e8f9c3ed6a7661e8f5d317b5554359c177
Emily Frost <emfrost@gmail.com>
2019-07-26 17:46:14 -0600

    functional

---
+++ README.md
@@ -0,0 +1,5 @@
+#Friend Finder
+
+Friend Finder is a fun way to meet your next BFF! Simply fill out the form, upload a photo, take the quiz and you'll be matched to someone whose answers overall most closely match your own.
+
+This app 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,11 +12,12 @@ module.exports = function(app) {
         res.send(friends)
     });
 
-    app.post("/api/friends/:friend", function(req, res) {
+    app.post("/api/friends", function(req, res) {
         friends.push(req.body);
-        var user = req.params.friend;
-        var totalDifference = 0; 
-        friends.forEach (function (friend) {
+        var user = req.body;
+        var totalDifference = 1000; 
+        var idealFriend = {};
+        friends.forEach (function (friend, index) {
             var userScores = user.scores;
             var friendsScores = friend.scores; 
             var scoreDifferences = [];
@@ -24,22 +25,48 @@ module.exports = function(app) {
                 var tempNum = (user.scores[i] - friend.scores[i]);
                 scoreDifferences.push(tempNum);
             }
-            scoreDifferences.reduce(function (total, currentValue) {
+            
+           var tempTotalDifference = scoreDifferences.reduce(function (total, currentValue) {
+                
                 return (total + currentValue)
-            })
-        })
+            }) 
+            if (tempTotalDifference < totalDifference) {
+                totalDifference = tempTotalDifference; 
+                idealFriend = {
+                    name: friend.name, 
+                    photo: friend.photo
+            }}
+        }) 
         //need to compare the total difference with something else to determine 
-        res.send(true);
+        res.json(idealFriend);
     })
 };  
+
+
+
+
+// Determine the user's most compatible friend using the following as a guide:
+
+// Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
+// With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
+
+// Example:
+// User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+// User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+// Total Difference: 2 + 1 + 2 = 5
+
+// Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
+// The closest match will be the user with the least amount of difference.
+
+// Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+
+// The modal should display both the name and picture of the closest match.
 
 
 //   // Displays a single character, or returns false
 //   app.get("/api/characters/:character", function(req, res) {
 //     var chosen = req.params.character;
-  
 //     console.log(chosen);
-  
 //     for (var i = 0; i < characters.length; i++) {
 //       if (chosen === characters[i].routeName) {
 //         return res.json(characters[i]);
